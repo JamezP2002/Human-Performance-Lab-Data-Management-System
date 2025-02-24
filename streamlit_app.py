da15584c3d6bf2e38122e8747208db1401152fd7
JamezP2002 <117998954+JamezP2002@users.noreply.github.com>
2025-02-24 10:58:33 -0500

    worked on pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,3 @@ st.title("Welcome to My Streamlit App")
 st.write("""
 This is the homepage of my Streamlit app. Use the sidebar to navigate to different pages.
 """)
-
-# Add a sidebar with navigation options
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Page 1", "Page 2", "Page 3"])
-
-# Define the content of each page
-if page == "Home":
-    st.write("You are on the home page.")
-elif page == "Page 1":
-    st.write("Welcome to Page 1!")
-elif page == "Page 2":
-    st.write("Welcome to Page 2!")
-elif page == "Page 3":
-    st.write("Welcome to Page 3!")
